75186f368a06bc24cee775c22d0dd5c7c5c9eb7d
Jeremy Bird <jbird@jbird-l.corp.instructure.com>
2018-03-03 12:41:20 -0700

    Jeremy commit - update movie index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,51 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+    @movies = Movie.all
   end
 
   def show
   end
 
   def new
+    @movie = Movie.new
   end
 
   def edit
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render  :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def movie_params
+      params.require(:move).permit(:title, :trailer)
+    end
 end
